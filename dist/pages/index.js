4f6b80cba48ae4d70d56088c89a409de9a003d6a
Kaushalendra Singh <yadavkausha4a5@gmail.com>
2025-08-21 08:44:08 +0530

    Update index.js

--- dist/pages/index.js
+++ dist/pages/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { schedular } from "../api/autopost.js";
 const app = express();
 app.use(express.json());
-app.use("/api/pages/autopost", schedular);
+app.use("/api/autopost", schedular);
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("We are home page");
